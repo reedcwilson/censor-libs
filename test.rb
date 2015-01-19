eaad0df0e9dde2c729c0bc340ba1aca6c428e08d
Reed Wilson <reedcwilson@gmail.com>
2015-01-19 11:22:11 -0700

    modified test

--- test.rb
+++ test.rb
@@ -1,4 +1,4 @@
 require './obscenity'
 
-puts Obscenity::Base.replacement('[censored]').sanitize('holy running. this is a test')
-puts Obscenity::Base.replacement('[censored]').sanitize('hello test. how the hell are running?')
+puts Obscenity.sanitize('holy running. this is a test')
+puts Obscenity.replacement('[censored]').sanitize('hello test. how the hell are running?')
